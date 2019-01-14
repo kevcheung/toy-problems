a8601a64c5f55c33716656236c8e4f3ca64d1e2f
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-01-14 16:00:15 -0600

    completed Equal Sides of an Array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//SUM OF THE FIRST NTH TERM OF SERIES
 //Your task is to write a function which returns the sum of following series upto nth term(parameter).
 
 //Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
@@ -21,6 +22,8 @@ function SeriesSum(n) {
     return sum.toFixed(2)
   }
 
+
+//DUBSTEP
 // Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
 
 // Let's assume that a song consists of some number of words (that don't contain WUB). To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
@@ -44,6 +47,8 @@ function songDecoder(song){
     return song.split("WUB").filter((el)=>el).join(' ');
   }
 
+
+//CATEGORIZE NEW MEMBER
 // The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
 // To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
@@ -76,3 +81,50 @@ function openOrSenior(data){
   let newData = [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
   
   openOrSenior(newData)
+
+
+//EQUAL SIDES OF AN ARRAY
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+// For example:
+
+// Let's say you are given the array {1,2,3,4,3,2,1}:
+// Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+// Let's look at another one.
+// You are given the array {1,100,50,-51,1,1}:
+// Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+// Last one:
+// You are given the array {20,10,-80,10,10,15,35}
+// At index 0 the left side is {}
+// The right side is {10,-80,10,10,15,35}
+// They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+// Index 0 is the place where the left side and right side are equal.
+
+// Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+// Input:
+// An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+// Output:
+// The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+
+// Note:
+// If you are given an array with multiple answers, return the lowest correct index.
+// An empty array should be treated like a 0 in this problem.
+
+function getSum(total, num){
+  return total + num
+}
+
+function findEvenIndex(arr){
+  let index = -1;
+  for (let i = 0; i < arr.length; i++){
+    // for (let j = arr.length; j > 0; j--){
+  if ((arr.slice(0,[i + 1])).reduce(getSum) === (arr.slice([i],arr.length)).reduceRight(getSum)){
+    index = i
+    }
+  }
+  return index
+}
