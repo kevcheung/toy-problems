63d73e1183acbcb82ff8814136ea9f7a94fbbb77
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-01-17 19:07:33 -0600

    started Sum of All Multiples of 3 or 5

--- index.js
+++ index.js
@@ -272,3 +272,15 @@ var isSquare = function(n){
 }
 
 isSquare(25)
+
+
+// SUM OF ALL THE MULTIPLES OF 3 OR 5
+// Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
