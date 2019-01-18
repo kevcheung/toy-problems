d29edd630a2698d7793f129472ecfc2da8d48b7b
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-01-17 18:26:12 -0600

    finished Roman Numerals Decoder

--- index.js
+++ index.js
@@ -218,3 +218,33 @@ function newLine(space, star){
 
 // Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
 
+function solution(roman){
+	let numerals = {
+	I : 1,
+	V : 5,
+	X : 10,
+	L : 50,
+	C : 100,
+	D : 500,
+	M : 1000
+	}
+	let arr = roman.split('');
+	let num = 0;
+  for (let i = 0; i < arr.length; i++){
+	let letter = numerals[arr[i]];
+	let nextLet = numerals[arr[i + 1]];
+		if (letter === undefined){
+			return null
+		}else{
+			if (letter < nextLet){
+        num += nextLet - letter
+        i++
+			}else{
+				num += letter
+			}
+		}
+	}
+	return num;
+}
+
+solution('XXI')
