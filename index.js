d3d287978ab493da2c0294854c4abea34582c346
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-02-07 12:43:18 -0600

    started Extract the domain name from a URL

--- index.js
+++ index.js
@@ -380,3 +380,11 @@ function tickets(arr){
 	}
 	return 'YES'
 }
+
+
+//EXTRACT THE DOMAIN NAME FROM A URL
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
