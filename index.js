2ba596d37688fd2b91689bf368fa591fdaf11d3a
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-02-13 17:33:29 -0600

    started Find the smallest

--- index.js
+++ index.js
@@ -415,3 +415,20 @@ function twoSum(numbers, target) {
 	}
 	return indices
 }
+
+
+// FIND THE SMALLEST
+// You have a positive number n consisting of digits. You can do at most one operation: Choosing the index of a digit in the number, remove this digit at that index and insert it back to another place in the number.
+
+// Doing so, find the smallest number you can get.
+
+// #Task: Return an array or a tuple or a string depending on the language (see "Sample Tests") with
+
+// 1) the smallest number you got
+// 2) the index i of the digit d you took, i as small as possible
+// 3) the index j (as small as possible) where you insert this digit d to have the smallest number.
+// Example:
+
+// smallest(261235) --> [126235, 2, 0] or (126235, 2, 0) or "126235, 2, 0"
+// 126235 is the smallest number gotten by taking 1 at index 2 and putting it at index 0
+
