99045fa72548d84094da69f2b58dae2cc78f4cdf
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-04-10 17:57:52 -0500

    started Getting along with Integer Partitions

--- index.js
+++ index.js
@@ -530,3 +530,7 @@ function duplicateEncode(word) {
 	}
 	return str
 }
+
+
+//GETTING ALONG WITH INTEGER PARTITIONS
+//In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition.
