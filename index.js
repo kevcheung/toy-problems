683f49321d83fa4d2bc783b1b4f5905b575b103c
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-04-11 17:17:28 -0500

    completed Alphabet War

--- index.js
+++ index.js
@@ -541,3 +541,28 @@ function duplicateEncode(word) {
 //ALPHABET WAR - AIRSTRIKE - LETTERS MASSACRE
 // Write a function that accepts "fight" string consists of only small letters and * which means a bomb drop place. Return who wins the fight after bombs are exploded. When the left side wins return "Left side wins!", when the right side wins return "Right side wins!", in other case return "Let's fight again!".
 
+function alphabetWar(fight) {
+	let letters = {
+		'w': 4,
+		'p': 3,
+		'b': 2,
+		's': 1,
+		'm': -4,
+		'q': -3,
+		'd': -2,
+		'z': -1
+	};
+	let fightArr = fight.split('');
+	let arr = [];
+	for (let i = 0; i < fightArr.length; i++) {
+		if (fightArr[i] !== '*' && fightArr[i + 1] !== '*' && fightArr[i - 1] !== '*') {
+			arr.push(fightArr[i])
+		}
+	}
+	let sum = arr.reduce((total, curr) => total + (letters[curr] ? letters[curr] : 0), 0);
+	return sum < 0
+		? "Right side wins!"
+		: sum > 0
+		? "Left side wins!"
+		: "Let's fight again!";
+}
