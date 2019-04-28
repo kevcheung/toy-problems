e075bbb85b5f436ecfa4b9abf0309a908d163fe5
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-04-27 19:20:18 -0500

    completed Multiples of 3 or 5

--- index.js
+++ index.js
@@ -689,3 +689,14 @@ function beggars(values, n){
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
+function solution(number){
+	let num = 0;
+	for(let i = 0; i < number; i++){
+		if((i % 3 === 0 && i % 5 === 0)||(i % 3 === 0||i % 5 === 0)){
+			num += i
+		}
+	}
+	return num
+}
+
+
