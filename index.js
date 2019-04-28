d3c1e927e74d28b1950c9b824887c213544116b8
Kevin Cheung <kevinchikitcheung@gmail.com>
2019-04-28 15:06:48 -0500

    started Find the Odd Int

--- index.js
+++ index.js
@@ -700,3 +700,6 @@ function solution(number){
 }
 
 
+//FIND THE ODD INT
+//Given an array, find the int that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
